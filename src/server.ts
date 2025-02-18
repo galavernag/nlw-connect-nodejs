df37f10e43ab5d28bf151f13af5e9c360d08b136
Guilherme Galaverna <guigalaverna@gmail.com>
2025-02-17 22:22:03 -0300

    feat: enable swagger

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,29 @@
 import { fastify } from "fastify";
 import { fastifyCors } from "@fastify/cors";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
 
 import {
   validatorCompiler,
   serializerCompiler,
   ZodTypeProvider,
+  jsonSchemaTransform,
 } from "fastify-type-provider-zod";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, { origin: "*" });
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "NLW Connect",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+app.register(fastifySwaggerUi, { routePrefix: "/docs" });
+
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
