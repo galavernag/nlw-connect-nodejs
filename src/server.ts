9758a969d1d600cb17d0d89ef47eedd79fed5b47
Guilherme Galaverna <guigalaverna@gmail.com>
2025-02-17 22:11:28 -0300

    feat: create first route

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import { fastify } from "fastify";
 
 const app = fastify();
+app.get("/hello", (_, reply) => {
+  reply.send("world");
+});
 
 app.listen({ port: 3333 }, () => {
   console.log("Server is runnning");
