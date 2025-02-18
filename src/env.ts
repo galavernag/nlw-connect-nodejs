fa2a675260c5a1b5c5d06c61e68cd522a118872c
Guilherme Galaverna <guigalaverna@gmail.com>
2025-02-18 18:27:05 -0300

    feat: add frontend url

--- src/env.ts
+++ src/env.ts
@@ -3,6 +3,8 @@ import z from 'zod'
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
 
+  WEB_URL: z.string().url(),
+
   POSTGRES_URL: z.string().url(),
   REDIS_URL: z.string().url(),
 })
