f8b8ea32b92ad257925b895901220b23d78e227b
Guilherme Galaverna <guigalaverna@gmail.com>
2025-02-17 22:11:42 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
 
 const app = fastify();
+
+app.register(fastifyCors, { origin: "*" });
+
 app.get("/hello", (_, reply) => {
   reply.send("world");
 });
